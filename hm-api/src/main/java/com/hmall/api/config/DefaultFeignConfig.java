b61570ff11c8ef4879f0b1bffcea829e0e7f4c99
lioyishiki <2460990660@qq.com>
2024-07-10 20:18:20 +0800

    gateway quick start

---
+++ hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
@@ -0,0 +1,15 @@
+package com.hmall.api.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @param
+ * @return
+ */
+public class DefaultFeignConfig {
+    @Bean
+    public Logger.Level feignLoggerLevel(){
+        return  Logger.Level.FULL;
+    }
+}
